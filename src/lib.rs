8a32bf46b66d7ec2b2b4ab611bea06d0ab2c00f9
tangentstorm <michal.wallace@gmail.com>
2018-12-09 21:46:16 -0500

    lib.rs: added some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
+//! A crate for working with boolean expressions.
+
 extern crate bincode;
 #[macro_use] extern crate serde_derive;
 extern crate fnv;
 extern crate hashbrown;
 
+/// The core library, and support for boolean expressions as abstract syntax trees.
 pub mod base;
 pub use base::*;
 
+/// Misc helper routines inspired by the APL family of programming languages.
 pub mod apl;
+/// Binary decision diagrams.
 pub mod bdd;
+/// Working with arrays of bit structures as if they were integers.
 pub mod x32;
+/// Input/output support for the other modules.
 pub mod io;
