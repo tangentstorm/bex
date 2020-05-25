2fac6d06c4648a22840ba14543f4c7300165fcc8
tangentstorm <michal.wallace@gmail.com>
2020-05-25 09:19:42 -0400

    update bench-solve

--- benches/bench-solve.rs
+++ benches/bench-solve.rs
@@ -5,13 +5,15 @@ use bencher::Bencher;
 extern crate bex;
 use bex::bdd;
 use bex::int::{BInt, BaseBit, GBASE, gbase_def};
-use bex::{find_factors, solve::{ProgressReport, bdd_refine, sort_by_cost}};
+use bex::{find_factors, solve::{ProgressReport, refine, sort_by_cost}};
 use bex::ast::ASTBase;
+
+type BDD = bdd::BDDBase;
 
 pub fn tiny(b: &mut Bencher) {
   use bex::int::{X4,X8};
   b.iter(|| {
-    find_factors!(X4,X8, 210, vec![(14,15)], false);
+    find_factors!(BDD, X4, X8, 210, vec![(14,15)], false);
   }); }
 
 pub fn small(b: &mut Bencher) {
@@ -19,7 +21,7 @@ pub fn small(b: &mut Bencher) {
   b.iter(|| {
     let expected = vec![(1,210), (2,105), ( 3,70), ( 5,42),
                         (6, 35), (7, 30), (10,21), (14,15)];
-    find_factors!(X8,X16, 210, expected, false);
+    find_factors!(BDD, X8, X16, 210, expected, false);
     GBASE.with(|gb| gb.replace(bex::ast::ASTBase::empty()));
   }); }
 
