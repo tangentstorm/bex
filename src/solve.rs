782b919e96bee6bee79544c86a0a8077adbfb02e
tangentstorm <michal.wallace@gmail.com>
2020-06-11 15:44:33 -0400

    solve: introduced SrcNid/DstNid to help clarify. Got nano tests to pass. (tiny still broken)

--- src/solve.rs
+++ src/solve.rs
@@ -12,9 +12,8 @@ type B = dyn Base<N=nid::NID>;
 
 pub trait Progress {
   fn on_start(&self);
-  fn on_step(&self, src:&ASTBase, dest: &mut B, step:usize, secs:u64,
-             oldtop:nid::NID, newtop:nid::NID);
-  fn on_done(&self, src:&ASTBase, dest: &mut B, newtop:nid::NID); }
+  fn on_step(&self, src:&ASTBase, dest: &mut B, step:usize, secs:u64, oldtop:DstNid, newtop:DstNid);
+  fn on_done(&self, src:&ASTBase, dest: &mut B, newtop:DstNid); }
 
 pub struct ProgressReport<'a> {
   pub save_dot: bool,
@@ -23,34 +22,39 @@ pub struct ProgressReport<'a> {
   pub show_result: bool,
   pub save_result: bool }
 
+/// these are wrappers so the type system can help us keep the src and dest nids separate
+#[derive(Clone, Copy, Debug, PartialEq)] pub struct SrcNid { pub n: nid::NID }
+#[derive(Clone, Copy, Debug, PartialEq)] pub struct DstNid { pub n: nid::NID }
+
 
 impl<'a> Progress for ProgressReport<'a> {
   fn on_start(&self) { } //println!("step, seconds, topnid, oldtopvar, newtopvar"); }
-  fn on_step(&self, src:&ASTBase, dest: &mut B, step:usize, secs:u64,
-             oldtop:nid::NID, newtop:nid::NID) {
-    println!("{:4}, {:4}, {:4}→{:3?}, {:8}",
-             step, secs, oldtop, src.get_op(oldtop), newtop);
+  fn on_step(&self, src:&ASTBase, dest: &mut B, step:usize, secs:u64, oldtop:DstNid, newtop:DstNid) {
+    let DstNid{ n: old } = oldtop;
+    let DstNid{ n: new } = newtop;
+    println!("{:4}, {:4}, {:4?}→{:3?}, {:8?}",
+             step, secs, oldtop, src.get_op(nid::nvi(nid::NOVAR, nid::idx(new) as u32)), newtop);
     if step.trailing_zeros() >= 3 { // every so often, save the state
       // !! TODO: expected number of steps only works if sort_by_cost was called.
       { let expected_steps = src.len() as f64;
         let percent_done = 100.0 * (step as f64) / expected_steps as f64;
-        println!("\n# newtop: {}  step:{}/{} ({:.2}%)",
+        println!("\n# newtop: {:?}  step:{}/{} ({:.2}%)",
                  newtop, step, src.len(), percent_done); }
       if self.save_dest {
-        dest.tag(newtop, "top".to_string()); dest.tag(nid::nv(step), "step".to_string());
+        dest.tag(new, "top".to_string()); dest.tag(nid::nv(step), "step".to_string());
         // TODO: remove the 'bdd' suffix
         dest.save(format!("{}-{:04}.bdd", self.prefix, step).as_str())
           .expect("failed to save"); }}
     if step.trailing_zeros() >= 5 { println!("step, seconds, change, newtop"); }
     if self.save_dot && (step.trailing_zeros() >= 5) || (step==446)
     { // on really special occasions, output a diagram
-      dest.save_dot(newtop, format!("{}-{:04}.dot", self.prefix, step).as_str()); } }
+      dest.save_dot(new, format!("{}-{:04}.dot", self.prefix, step).as_str()); } }
 
-  fn on_done(&self, _src:&ASTBase, dest: &mut B, newtop:nid::NID) {
+  fn on_done(&self, _src:&ASTBase, dest: &mut B, newtop:DstNid) {
     if self.show_result {
-      dest.show_named(newtop, format!("{}-final", self.prefix).as_str()); }
+      dest.show_named(newtop.n, format!("{}-final", self.prefix).as_str()); }
     else if self.save_result {
-      dest.save_dot(newtop, format!("{}-final.dot", self.prefix).as_str()); }
+      dest.save_dot(newtop.n, format!("{}-final.dot", self.prefix).as_str()); }
     else {}}}
 
 
@@ -61,7 +65,7 @@ fn default_bitmask(_src:&ASTBase, v:nid::VID) -> u64 {
 /// Sorting your AST this way dramatically reduces the cost of converting to
 /// another form. (For example, the test_tiny benchmark drops from 5282 steps to 111 for BDDBase)
 #[allow(clippy::needless_range_loop)]
-pub fn sort_by_cost(src:&ASTBase, top:ast::NID)->(ASTBase,ast::NID) {
+pub fn sort_by_cost(src:&ASTBase, top:SrcNid)->(ASTBase,SrcNid) {
   todo!("rebuild sort_by_cost()")
   /*
   let (mut src0,kept0) = src.repack(vec![top]);
@@ -82,53 +86,56 @@ pub fn sort_by_cost(src:&ASTBase, top:ast::NID)->(ASTBase,ast::NID) {
   (ast,nid)*/ }
 
 
-pub fn refine<P:Progress>(dest: &mut B, src:&ASTBase, end:nid::NID, pr:P)->nid::NID {
-  let mut topnid = convert_nid(end);
-  println!("INITIAL TOPNID: {:?}", topnid);
+pub fn refine<P:Progress>(dest: &mut B, src:&ASTBase, end:DstNid, pr:P)->DstNid {
+  // end is the root of the expression to simplify, as a nid in the src ASTbase.
+  // we want its equivalent expression in the dest base:
+  let mut top = end;
+  println!("INITIAL TOPNID: {:?}", top);
   // step is just a number. we're packing it in a nid as a kludge
   let mut step = nid::var(dest.get(&"step".to_string()).unwrap_or_else(||nid::nv(0)));
   pr.on_start();
-  while !(nid::is_rvar(topnid) || nid::is_const(topnid)) {
+  while !(nid::is_rvar(top.n) || nid::is_const(top.n)) {
     let now = std::time::SystemTime::now();
-    let oldtop = topnid;
-    topnid = refine_one(dest, &src, oldtop);
+    let oldtop = top;
+    top = refine_one(dest, &src, oldtop);
+    assert!(oldtop != top, "top should have changed!");
     let secs = now.elapsed().expect("elapsed?").as_secs();
-    pr.on_step(src, dest, step, secs, oldtop, topnid);
+    pr.on_step(src, dest, step, secs, oldtop, top);
     step += 1; }
-  pr.on_done(src, dest, topnid);
-  topnid }
+  pr.on_done(src, dest, top);
+  top }
 
 /// map a nid from the source to a (usually virtual) variable in the destination
-pub fn convert_nid(n:ast::NID)->nid::NID {
-  if nid::is_const(n) { n }
-  else if nid::is_var(n) { nid::nvr(nid::var(n)) }
-  else if nid::var(n) == nid::NOVAR { nid::nv(nid::idx(n)) }
+pub fn convert_nid(sn:SrcNid)->DstNid {
+  let SrcNid{ n } = sn;
+  if nid::is_const(n) { DstNid{ n } }
+  else if nid::is_var(n) { DstNid{ n: nid::nvr(nid::var(n)) } }
+  else if nid::var(n) == nid::NOVAR { DstNid{ n: nid::nv(nid::idx(n)) }}
   else { todo!("convert_nid({:?})", n) }}
 
-fn refine_one(dst: &mut B, src:&ASTBase, n:nid::NID)->nid::NID {
-  println!("refine_one({:?})", n);
-  if nid::is_const(n) { n }
-  else if nid::is_var(n) { nid::nvr(nid::var(n)) }
-  else if nid::var(n) == nid::NOVAR {
-    let otv = nid::idx(n);
-    let op = src.get_op(n);
-    let v = |x0:ast::NID|->nid::NID { convert_nid(x0) };
-    println!("op: {:?}", op);
+/// replace a
+fn refine_one(dst: &mut B, src:&ASTBase, d:DstNid)->DstNid {
+  // println!("refine_one({:?})", d);
+  if nid::is_const(d.n) { d }
+  else {
+    let otv = nid::var(d.n);
+    let op = src.get_op(nid::nvi(nid::NOVAR, otv as u32));
+    let cn = |x0:nid::NID|->nid::NID { convert_nid(SrcNid{n:x0}).n };
+    // println!("op: {:?}", op);
     let newdef:nid::NID = match op {
       // Op::Not should only occur once at the very top, if at all:
-      Op::Not(x) => dst.not(v(x)),
+      Op::Not(x) => dst.not(cn(x)),
       // the VIDs on the right here are because we're treating each step in the
       // calculation as a 'virtual' input variable, and just slowly simplifying
       // until the virtual variables are all gone.
-      Op::And(x,y) => dst.and(v(x), v(y)),
-      Op::Xor(x,y) => dst.xor(v(x), v(y)),
-      Op::Or(x,y) => dst.or(v(x), v(y)),
+      Op::And(x,y) => dst.and(cn(x), cn(y)),
+      Op::Xor(x,y) => dst.xor(cn(x), cn(y)),
+      Op::Or(x,y) => dst.or(cn(x), cn(y)),
       // !! 'Var' should only appear in leaves, so don't need it here.
-      // Op::Var(x) => nid::nvr(x as nid::VID),
+      //Op::Var(x) => nid::nvr(x as nid::VID),
       _ => { panic!("don't know how to translate {:?}", op ) }};
-    println!("sub(otv:{:?}, new:{:?}, old:{:?})", otv, newdef, n);
-    dst.sub(otv, newdef, n) }
-  else { todo!("refine_one({:?})", n) }}
+    // println!("sub(otv:{:?}, new:{:?}, old:{:?})", otv, newdef, d);
+    DstNid{n: dst.sub(otv, newdef, d.n) }}}
 
 
 /// This is an example solver used by the bdd-solve example and the bench-solve benchmark.
@@ -137,7 +144,7 @@ fn refine_one(dst: &mut B, src:&ASTBase, n:nid::NID)->nid::NID {
 #[macro_export]
 macro_rules! find_factors {
   ($TDEST:ident, $T0:ident, $T1:ident, $k:expr, $expect:expr, $show:expr) => {{
-    use bex::Base;
+    use bex::{Base,nid, solve::{SrcNid, DstNid, convert_nid}};
     // reset gbase on each test
     GBASE.with(|gb| gb.replace(ASTBase::empty()));
     let x = $T0::def("x");
@@ -148,19 +155,21 @@ macro_rules! find_factors {
       GBASE.with(|gb| { gb.borrow().show_named(eq.clone().n, "eq") }); }
     let top:BaseBit = lt & eq;
     let mut dest = $TDEST::new(nid::idx(top.n));
-    let answer = GBASE.with(|gb| {
+    let answer:DstNid = GBASE.with(|gb| {
       //let (src, newtop) = sort_by_cost(&gb.borrow(), nid::un(top.n));
-      let (src, newtop) = (gb.borrow(), top.n);
+      let (src, newtop) = (gb.borrow(), convert_nid(SrcNid{n: top.n}));
       // The diagram looks exactly the same before and after sort_by_cost, so I
       // only generate it once. The only difference is the internal numbering.
       // However: this sorting dramatically reduces the cost of the conversion.
       // For example, test_tiny drops from to 111 steps.
-      if $show { src.show_named(newtop, "ast"); }
+      if $show { src.show_named(newtop.n, "ast"); }
       dest = $TDEST::new(src.len());
       refine(&mut dest, &src, newtop,
              ProgressReport{ save_dot: $show, save_dest: false, prefix: "x",
                              show_result: $show, save_result: $show }) });
+    println!("done with refinement!");
     let expect = $expect;
+    let answer = answer.n;
     let actual:Vec<(u64, u64)> = dest.nidsols_trunc(answer, 2*$T0::n() as usize).map(|nids| {
       let mut res = (0, 0);
       let mut it = nids.iter();
@@ -183,7 +192,8 @@ macro_rules! find_factors {
   let x = X2::def("x"); let y = X2::def("y");
   let xy:X4 = x.times(&y); let k = X4::new(6);
   let lt = x.lt(&y); let eq = xy.eq(&k);
-  GBASE.with(|gb| { gb.borrow().show_named(eq.clone().n, "eq") });
-  let top:BaseBit = lt & eq;
-
+  let top = lt.clone() & eq.clone();
+  //GBASE.with(|gb| { gb.borrow().show_named(lt.clone().n, "lt") });
+  //GBASE.with(|gb| { gb.borrow().show_named(eq.clone().n, "eq") });
+  //GBASE.with(|gb| { gb.borrow().show_named(top.clone().n, "top") });
 }
