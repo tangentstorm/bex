c226f33a057373f3b0ed40403d73d1b05e730249
tangentstorm <michal.wallace@gmail.com>
2023-03-19 21:57:18 -0400

    bdd_swarm: moved worker methods from mod level to impl

--- src/bdd/bdd_swarm.rs
+++ src/bdd/bdd_swarm.rs
@@ -41,11 +41,51 @@ impl swarm::Worker<Q,R> for BddWorker {
   fn work_step(&mut self, _qid:&QID, q:Q)->Option<R> {
     match q {
       Q::Init(s) => { self.state = Some(s); None }
-      Q::Ite(ite) => { Some(swarm_ite(self.state.as_ref().unwrap(), ite)) }
+      Q::Ite(ite) => { Some(self.ite(ite)) }
       Q::Stats => {
         let tests = COUNT_XMEMO_TEST.with(|c| c.replace(0));
         let fails = COUNT_XMEMO_FAIL.with(|c| c.replace(0));
         Some(R::MemoStats{ tests, fails }) } }}}
+
+/// Code run by each thread in the swarm. Isolated as a function without channels for testing.
+impl BddWorker {
+  fn ite(&self, ite0:ITE)->R {
+    let ITE { i, t, e } = ite0;
+    match ITE::norm(i,t,e) {
+        Norm::Nid(n) => R::Nid(n),
+        Norm::Ite(ite) => self.ite_norm(ite),
+        Norm::Not(ite) => !self.ite_norm(ite) }}
+
+  fn vhl_norm(&self, ite:ITE)->R {
+    let ITE{i:vv,t:hi,e:lo} = ite; let v = vv.vid();
+    if let Some(n) = self.state.as_ref().unwrap().get_simple_node(v, HiLo{hi,lo}) {
+      R::Nid(n) }
+    else { R::Vhl{ v, hi, lo, invert:false } }}
+
+  fn ite_norm(&self, ite:ITE)->R {
+    let ITE { i, t, e } = ite;
+    let (vi, vt, ve) = (i.vid(), t.vid(), e.vid());
+    let v = ite.top_vid(); let state = self.state.as_ref().unwrap();
+    match state.get_memo(&ite) {
+      Some(n) => R::Nid(n),
+      None => {
+        let (hi_i, lo_i) = if v == vi {state.tup(i)} else {(i,i)};
+        let (hi_t, lo_t) = if v == vt {state.tup(t)} else {(t,t)};
+        let (hi_e, lo_e) = if v == ve {state.tup(e)} else {(e,e)};
+        // now construct and normalize the queries for the hi/lo branches:
+        let hi = ITE::norm(hi_i, hi_t, hi_e);
+        let lo = ITE::norm(lo_i, lo_t, lo_e);
+        // if they're both simple nids, we're guaranteed to have a vhl, so check cache
+        if let (Norm::Nid(hn), Norm::Nid(ln)) = (hi,lo) {
+          match ITE::norm(NID::from_vid(v), hn, ln) {
+            // first, it might normalize to a nid directly:
+            // !! but wait. how is this possible? i.is_const() and v == fake variable "T"?
+            Norm::Nid(n) => { R::Nid(n) }
+            // otherwise, the normalized triple might already be in cache:
+            Norm::Ite(ite) => self.vhl_norm(ite),
+            Norm::Not(ite) => !self.vhl_norm(ite)}}
+        // otherwise at least one side is not a simple nid yet, and we have to defer
+        else { R::Wip{ v, hi, lo, invert:false } }}}} }
 
 
 // ----------------------------------------------------------------
@@ -190,41 +230,3 @@ impl BddSwarm {
     COUNT_XMEMO_FAIL.with(|c| *c.borrow_mut() += fails ); }
 
 } // end bddswarm
-
-/// Code run by each thread in the swarm. Isolated as a function without channels for testing.
-fn swarm_ite(state: &Arc<BddState>, ite0:ITE)->R {
-  let ITE { i, t, e } = ite0;
-  match ITE::norm(i,t,e) {
-      Norm::Nid(n) => R::Nid(n),
-      Norm::Ite(ite) => swarm_ite_norm(state, ite),
-      Norm::Not(ite) => !swarm_ite_norm(state, ite) }}
-
-fn swarm_vhl_norm(state: &Arc<BddState>, ite:ITE)->R {
-  let ITE{i:vv,t:hi,e:lo} = ite; let v = vv.vid();
-  if let Some(n) = state.get_simple_node(v, HiLo{hi,lo}) { R::Nid(n) }
-  else { R::Vhl{ v, hi, lo, invert:false } }}
-
-fn swarm_ite_norm(state: &Arc<BddState>, ite:ITE)->R {
-  let ITE { i, t, e } = ite;
-  let (vi, vt, ve) = (i.vid(), t.vid(), e.vid());
-  let v = ite.top_vid();
-  match state.get_memo(&ite) {
-    Some(n) => R::Nid(n),
-    None => {
-      let (hi_i, lo_i) = if v == vi {state.tup(i)} else {(i,i)};
-      let (hi_t, lo_t) = if v == vt {state.tup(t)} else {(t,t)};
-      let (hi_e, lo_e) = if v == ve {state.tup(e)} else {(e,e)};
-      // now construct and normalize the queries for the hi/lo branches:
-      let hi = ITE::norm(hi_i, hi_t, hi_e);
-      let lo = ITE::norm(lo_i, lo_t, lo_e);
-      // if they're both simple nids, we're guaranteed to have a vhl, so check cache
-      if let (Norm::Nid(hn), Norm::Nid(ln)) = (hi,lo) {
-        match ITE::norm(NID::from_vid(v), hn, ln) {
-          // first, it might normalize to a nid directly:
-          // !! but wait. how is this possible? i.is_const() and v == fake variable "T"?
-          Norm::Nid(n) => { R::Nid(n) }
-          // otherwise, the normalized triple might already be in cache:
-          Norm::Ite(ite) => swarm_vhl_norm(state, ite),
-          Norm::Not(ite) => !swarm_vhl_norm(state, ite)}}
-      // otherwise at least one side is not a simple nid yet, and we have to defer
-      else { R::Wip{ v, hi, lo, invert:false } }}}}
