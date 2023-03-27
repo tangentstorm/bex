766544849bfa09cb8c23b461a351a96253120052
tangentstorm <michal.wallace@gmail.com>
2023-03-27 17:19:48 -0400

    add build.rs and factor-p4-bytes

---
+++ examples/solve/factor-p4-bytes.rs
@@ -0,0 +1,32 @@
+//! this program factors primorial 4 (210 = */2 3 5 7)
+//! into two bytes x,y where that x<y.
+
+/// Product of the first 4 primes: 2 3 5 7
+const K:usize = 210;
+
+fn factors()->Vec<(u64,u64)> {
+  vec![(1,210), (2,105), ( 3,70), ( 5,42),
+       (6, 35), (7, 30), (10,21), (14,15)]}
+
+extern crate bex;
+extern crate hashbrown;
+use hashbrown::HashSet;
+use bex::{find_factors, Base, bdd::BddBase, int::{X8,X16}, swap::SwapSolver, reg::Reg};
+
+include!(concat!(env!("OUT_DIR"), "/bex-build-info.rs"));
+
+pub fn main() {
+  let expected = factors();
+  let mut use_swap = false;
+  let mut get_threads = false; let mut num_threads = 0;
+  for a in std::env::args() {
+    if get_threads { num_threads = a.parse().expect("bad -t parameter"); get_threads=false; }
+    else { match a.as_str() {
+      "-t" => get_threads = true,
+      "swap" => use_swap = true,
+      _ => { /* ignore for now */} }}}
+  println!("[bex {BEX_VERSION} -O{BEX_OPT_LEVEL}] factor-p4 ({})",
+  if use_swap { "swap solver" } else { "sub solver" });
+  if num_threads != 0 { println!("note: t parameter is ignored"); }
+  if use_swap { find_factors!(SwapSolver, X8, X16, K as usize, expected); }
+  else { find_factors!(BddBase, X8, X16, K as usize, expected); }}
