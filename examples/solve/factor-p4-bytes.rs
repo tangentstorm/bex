37a6c29d34b49940cd0793cce55688bd64c2e8ab
tangentstorm <michal.wallace@gmail.com>
2023-03-27 12:10:52 -0400

    factor-p4: add '-t THREADS' and 'swap' cli args

--- examples/solve/factor-p4-bytes.rs
+++ examples/solve/factor-p4-bytes.rs
@@ -9,13 +9,23 @@ fn factors()->Vec<(u64,u64)> {
        (6, 35), (7, 30), (10,21), (14,15)]}
 
 extern crate bex;
-use bex::{solve::find_factors, bdd::BddBase, int::{X8,X16}};
+use bex::{solve::find_factors, bdd::BddBase, int::{X8,X16}, swap::SwapSolver};
 
 include!(concat!(env!("OUT_DIR"), "/bex-build-info.rs"));
 
 pub fn main() {
-  println!("bex {BEX_VERSION} opt-level: {BEX_OPT_LEVEL}");
   let expected = factors();
-  find_factors::<X8, X16, BddBase>(&mut BddBase::new_with_threads(0), K, expected);
-  //  find_factors!(SwapSolver, X8, X16, K, expected);
- }
+  let mut use_swap = false;
+  let mut get_threads = false; let mut num_threads = 0;
+  for a in std::env::args() {
+    if get_threads { num_threads = a.parse().expect("bad -t paremeter"); get_threads=false; }
+    else { match a.as_str() {
+      "-t" => get_threads = true,
+      "swap" => use_swap = true,
+      _ => { /* ignore for now */} }}}
+  println!("[bex {BEX_VERSION} -O{BEX_OPT_LEVEL}] factor-p4 -t {num_threads} ({})",
+    if use_swap { "swap solver" } else { "sub solver" });
+  if use_swap {
+    if num_threads != 0 { println!("note: swap solver ignores -t parameter"); }
+    find_factors::<X8, X16, SwapSolver>(&mut SwapSolver::new(), K, expected); }
+  else { find_factors::<X8, X16, BddBase>(&mut BddBase::new_with_threads(num_threads), K, expected); }}
