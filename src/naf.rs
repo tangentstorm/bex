fc81473670c0c3c19b4f519b4a01e652b476dab5
tangentstorm <michal.wallace@gmail.com>
2023-11-07 12:00:22 -0500

    naf: more skeleton code for AST->NAF translation

--- src/naf.rs
+++ src/naf.rs
@@ -6,7 +6,7 @@
 use crate::{NID, vid::VID};
 use crate::ast::RawASTBase;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum NAF {
   Vhl { v: VID, hi:NID, lo:NID },
   And { x: NID, y: NID },
@@ -27,13 +27,30 @@ impl NAFBase {
     self.nodes.push(x);
     res }
 
-  pub fn and(&mut self, x:NID, y:NID)->NID {
-    // !! assume the AST was already simplified, so no simple cases
-    self.push(NAF::And{ x, y}) }
+  pub fn get(&self, n:NID)->Option<NAF> {
+    assert!(!n.is_const(), "const nids should be optimized out by now");
+    if n.is_ixn() {
+      assert!(!n.is_inv(), "can't fetch inverted nids");
+      self.nodes.get(n.idx()).cloned() }
+    // !! todo: uncomment to start building Vhl nodes up from variables.
+    // else if n.is_var() { Some(NAF::Vhl { v: n.vid(), hi:NID::i(), lo: NID::o()}) }
+    else { None }}
 
-  pub fn xor(&mut self, x:NID, y:NID)->NID {
+  pub fn and(&mut self, xi:NID, yi:NID)->NID {
+    // !! assume the AST was already simplified, so no simple cases
+    if let (Some(x), Some(y)) = (self.get(xi.raw()), self.get(yi.raw())) {
+      if let (NAF::Vhl { v:_xv, hi:_xh, lo:_xl },
+              NAF::Vhl { v:_yv, hi:_yh, lo:_yl }) = (x, y) {
+          todo!("implement AND. Handle .is_inv bits")}}
+    self.push(NAF::And{ x:xi, y:yi }) }
+
+  pub fn xor(&mut self, xi:NID, yi:NID)->NID {
     // !! again, assume AST is pre-simplified
-    self.push(NAF::Xor{ x, y}) }
+    if let (Some(x), Some(y)) = (self.get(xi.raw()), self.get(yi.raw())) {
+      if let (NAF::Vhl { v:_xv, hi:_xh, lo:_xl },
+              NAF::Vhl { v:_yv, hi:_yh, lo:_yl }) = (x, y) {
+          todo!("implement XOR. Handle .is_inv bits")}}
+    self.push(NAF::Xor{ x:xi, y:yi }) }
 
   // return the definition of the topmost node in the translated AST
   pub fn top(&self)->Option<&NAF> { self.nodes.last().clone() }}
