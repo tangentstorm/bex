6525f7704631df31f404eccb8ee5e621632ce594
tangentstorm <michal.wallace@gmail.com>
2023-11-08 17:00:39 -0500

    ops: added some named 'signal' constants

--- src/ops.rs
+++ src/ops.rs
@@ -2,9 +2,9 @@
 use crate::{NID, Fun, nid::NidFun, vid::VID};
 use std::slice::Iter;
 
-///! A sequence of operations.
-///! Currently, RPN is the only format, but I made this an enum
-///! to provide a little future-proofing.
+/// A sequence of operations.
+/// Currently, RPN is the only format, but I made this an enum
+/// to provide a little future-proofing.
 #[derive(PartialOrd, PartialEq, Eq, Hash, Debug, Clone)]
 pub enum Ops { RPN(Vec<NID>) }
 impl Ops {
@@ -42,20 +42,71 @@ impl Ops {
 /// constructor for rpn
 pub fn rpn(xs:&[NID])->Ops { Ops::RPN(xs.to_vec()) }
 
+pub mod sig {
+
+  macro_rules! signals {
+    ($($ids:ident : $exs:expr),+ $(,)?) => { signals![@ $($ids : $exs),+]; };
+    (@) => {};
+    (@ $id:ident : $ex:expr $(, $ids:ident : $exs:expr)*) => {
+      pub const $id:u32 = $ex;
+      signals![@ $($ids : $exs),*]; };}
+
+  signals! {
+    // constant signals
+    K0:0x00000000, K1:0xffffffff,
+
+    // input bit signals (raw and inverted)
+    A:0x55555555, RX0:A, NX0:!A,
+    B:0x33333333, RX1:B, NX1:!B,
+    C:0x0f0f0f0f, RX2:C, NX2:!C,
+    D:0x00ff00ff, RX3:D, NX3:!D,
+    E:0x0000ffff, RX4:E, NX4:!E,
+
+    // NB. all possible 5-bit anf terms (except the singleton terms above)
+    // ] (,': ',([,'&',])/,','"_)S:0 terms=:(#~1<#S:0)/:~'ABCDE'{~L:0<@I.#:i.32
+    AB: A&B,        ABC: A&B&C,     ABCD: A&B&C&D,  ABCDE: A&B&C&D&E,
+    ABCE: A&B&C&E,  ABD: A&B&D,     ABDE: A&B&D&E,  ABE: A&B&E,
+    AC: A&C,        ACD: A&C&D,     ACDE: A&C&D&E,  ACE: A&C&E,
+    AD: A&D,        ADE: A&D&E,     AE: A&E,        BC: B&C,
+    BCD: B&C&D,     BCDE: B&C&D&E,  BCE: B&C&E,     BD: B&D,
+    BDE: B&D&E,     BE: B&E,        CD: C&D,        CDE: C&D&E,
+    CE: C&E,        DE: D&E,
+
+    // same ANF terms but now written in "big endian" style
+    // ([,': ',],','"_)/@:>"1(\:~L:0 terms),.terms
+    BA: AB,       CBA: ABC,    DCBA: ABCD,   EDCBA: ABCDE,
+    ECBA: ABCE,   DBA: ABD,    EDBA: ABDE,   EBA: ABE,
+    CA: AC,       DCA: ACD,    EDCA: ACDE,   ECA: ACE,
+    DA: AD,       EDA: ADE,    EA: AE,       CB: BC,
+    DCB: BCD,     EDCB: BCDE,  ECB: BCE,     DB: BD,
+    EDB: BDE,     EB: BE,      DC: CD,       EDC: CDE,
+    EC: CE,       ED: DE,
+
+    // the (non-degenerate) dyadic boolean functions
+    AND: A&B,    NAND: !(A&B),
+    XOR: A^B,    IFF: !XOR,
+    OR: A|B,     NOR: !OR,
+    LT: !A&B,    GT: A&!B,
+    LTE: !GT,    GTE: !LT,
+
+    // and some aliases:
+    VEL:OR,     IMP: LTE,
+    NE: XOR,    EQ: IFF }}
+
 /// x0 & x1
-pub const AND:NidFun = NID::fun(2,0b0001);
+pub const AND:NidFun = NID::fun(2,sig::AND);
 
 /// x0 ^ x1
-pub const XOR:NidFun = NID::fun(2,0b0110);
+pub const XOR:NidFun = NID::fun(2,sig::XOR);
 
 /// x0 | x1   (vel is the latin word for 'inclusive or', and the origin of the "∨" symbol in logic)
-pub const VEL:NidFun = NID::fun(2,0b0111);
+pub const VEL:NidFun = NID::fun(2,sig::VEL);
 
 /// !(x0 | x1)
-pub const NOR:NidFun = NID::fun(2,0b1000);
+pub const NOR:NidFun = NID::fun(2,sig::NOR);
 
 /// x0 implies x1  (x0 <= x1)
-pub const IMP:NidFun = NID::fun(2,0b1011);
+pub const IMP:NidFun = NID::fun(2,sig::IMP);
 
 /// convenience trait that allows us to mix vids and nids
 /// freely when constructing expressions.
