5dfdc5077d52750c8893261d52409f35e072f329
tangentstorm <michal.wallace@gmail.com>
2025-02-09 14:05:00 -0500

    py: AST->ASTBase, simplify structs

--- py/src/lib.rs
+++ py/src/lib.rs
@@ -4,9 +4,9 @@ use pyo3::prelude::*;
 use pyo3::exceptions::PyException;
 use bex_rs::{Base, GraphViz, ast::ASTBase, nid::{I,O,NID}, vid::VID};
 
-#[pyclass(name="NID")] struct PyNID{ nid:NID }
-#[pyclass(name="VID")] struct PyVID{ vid:VID }
-#[pyclass(name="AST")] struct PyAST { base: ASTBase }
+#[pyclass(name="NID")] struct PyNID(NID);
+#[pyclass(name="VID")] struct PyVID(VID);
+#[pyclass(name="ASTBase")] struct PyASTBase(ASTBase);
 
 enum BexErr { NegVar, NegVir }
 impl std::convert::From<BexErr> for PyErr {
@@ -17,39 +17,35 @@ impl std::convert::From<BexErr> for PyErr {
 
 #[pymethods]
 impl PyNID {
-  #[staticmethod]
-  fn var(i:i32)->PyResult<Self> { if i<0 { Err(BexErr::NegVar.into()) } else { Ok(PyNID{ nid:NID::var(i as u32)}) }}
-  #[staticmethod]
-  fn vir(i:i32)->PyResult<Self> { if i<0 { Err(BexErr::NegVir.into()) } else { Ok(PyNID{ nid:NID::vir(i as u32)}) }}
-  fn __str__(&self) -> String { self.nid.to_string() }
-  fn __repr__(&self) -> String { format!("<NID({:?})>", self.nid) }}
+  fn is_const(&self)->bool { self.0.is_const() }
+  fn __str__(&self) -> String { self.0.to_string() }
+  fn __repr__(&self) -> String { format!("<NID({:?})>", self.0) }}
 
 #[pymethods]
 impl PyVID {
-  fn __str__(&self) -> String { self.vid.to_string() }
-  fn __repr__(&self) -> String { format!("<VID({:?})>", self.vid) }}
+  fn __str__(&self) -> String { self.0.to_string() }
+  fn __repr__(&self) -> String { format!("<VID({:?})>", self.0) }}
 
 #[pymethods]
-impl PyAST {
-  #[new] fn __new__()->Self { Self{ base: ASTBase::empty() }}
-  fn op_and(&mut self, x:&PyNID, y:&PyNID)->PyNID { PyNID{ nid:self.base.and(x.nid, y.nid) }}
-  fn op_xor(&mut self, x:&PyNID, y:&PyNID)->PyNID { PyNID{ nid:self.base.xor(x.nid, y.nid) }}
-  fn op_or(&mut self, x:&PyNID, y:&PyNID)->PyNID  { PyNID{ nid:self.base.or(x.nid, y.nid) }}
-  fn to_dot(&self, x:&PyNID)->String { let mut s = String::new(); self.base.write_dot(x.nid, &mut s); s }}
+impl PyASTBase {
+  #[new] fn __new__()->Self { Self(ASTBase::empty()) }
+  fn op_and(&mut self, x:&PyNID, y:&PyNID)->PyNID { PyNID(self.0.and(x.0, y.0)) }
+  fn op_xor(&mut self, x:&PyNID, y:&PyNID)->PyNID { PyNID(self.0.xor(x.0, y.0)) }
+  fn op_or(&mut self, x:&PyNID, y:&PyNID)->PyNID  { PyNID(self.0.or(x.0, y.0)) }
+  fn to_dot(&self, x:&PyNID)->String { let mut s = String::new(); self.0.write_dot(x.0, &mut s); s }}
 
 #[pyfunction]
-fn var(i:i32)->PyResult<PyNID> { PyNID::var(i) }
-
+fn var(i:i32)->PyResult<PyNID> { if i<0 { Err(BexErr::NegVar.into()) } else { Ok(PyNID(NID::var(i as u32))) }}
 #[pyfunction]
-fn vir(i:i32)->PyResult<PyNID> { PyNID::vir(i) }
+fn vir(i:i32)->PyResult<PyNID> { if i<0 { Err(BexErr::NegVir.into()) } else { Ok(PyNID(NID::vir(i as u32))) }}
 
 #[pymodule]
 fn bex(m: &Bound<'_, PyModule>)->PyResult<()> {
   m.add_class::<PyVID>()?;
   m.add_class::<PyNID>()?;
-  m.add_class::<PyAST>()?;
-  m.add("O", PyNID{nid:O})?;
-  m.add("I", PyNID{nid:I})?;
+  m.add_class::<PyASTBase>()?;
+  m.add("O", PyNID(O))?;
+  m.add("I", PyNID(I))?;
 
   m.add_function(wrap_pyfunction!(var, m)?)?;
   m.add_function(wrap_pyfunction!(vir, m)?)?;
--- py/test.py
+++ py/test.py
@@ -1,4 +1,4 @@
-from bex import I, O, vir, var, AST
+from bex import I, O, vir, var, ASTBase
 assert str(O)=="O"
 assert str(I)=="I"
 assert str(vir(0))=="v0"
@@ -6,7 +6,7 @@ assert str(var(0))=="x0"
 
 x0, x1, x2 = [var(x) for x in range(3)]
 
-base = AST()
+base = ASTBase()
 n0 = base.op_and(x0, x1)
 n1 = base.op_or(x2, n0)
 
