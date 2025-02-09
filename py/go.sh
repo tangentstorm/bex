132c9bc565758fe519480c3a378f3f94515a9eeb
tangentstorm <michal.wallace@gmail.com>
2025-02-09 14:24:14 -0500

    py: clean up shell scripts

--- py/go.sh
+++ py/go.sh
@@ -1,4 +1,6 @@
-cd py
+if [ "${PWD##*/}" != "py" ]; then
+  cd py
+fi
 export PY=c:/python311
 maturin build -i $PY/python.exe \
 && $PY/scripts/pip install --force-reinstall ../target/wheels/pybex-0.2.0-cp311-cp311-win_amd64.whl \
